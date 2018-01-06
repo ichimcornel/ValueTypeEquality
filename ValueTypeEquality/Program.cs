9b1b66b7453c4b40bbad9c82c8d6683fddfe7fe7
Cornel Ichim <ichimcornel@ymail.com>
2018-01-06 16:07:06 +0200

    Add project files.

---
+++ ValueTypeEquality/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValueTypeEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
